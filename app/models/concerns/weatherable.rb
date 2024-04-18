c3bad44c0d8f91edbddcccbdb66e014938f0beaf
Spike Vinz Cruz <spikevinz@gmail.com>
2024-04-18 14:44:19 +0800

    refactor weaterable concern

--- app/models/concerns/weatherable.rb
+++ app/models/concerns/weatherable.rb
@@ -6,32 +6,28 @@ module Weatherable
   included do
     def current
       Weather.new(
-        code: data[:weather_code][0],
-        date: data[:time][0],
-        temperature: data[:temperature_2m_max][0],
-        wind: data[:wind_speed_10m_max][0]
+        code: weather_data['weather_code'][0],
+        date: weather_data['time'][0],
+        temperature: weather_data['temperature_2m_max'][0],
+        wind: weather_data['wind_speed_10m_max'][0]
       )
     end
 
     def upcoming
       (1..6).map do |index|
         Weather.new(
-          code: data[:weather_code][index],
-          date: data[:time][index],
-          temperature: data[:temperature_2m_max][index],
-          wind: data[:wind_speed_10m_max][index]
+          code: weather_data['weather_code'][index],
+          date: weather_data['time'][index],
+          temperature: weather_data['temperature_2m_max'][index],
+          wind: weather_data['wind_speed_10m_max'][index]
         )
       end
     end
 
     def weather_outdated?
-      data[:time].first != Date.today.to_s
-    end
+      return true if weather_data.nil?
 
-    private
-
-    def data
-      weather_data.with_indifferent_access
+      weather_data['time'].first != Date.today.to_s
     end
   end
 end
