6427e36aa7f5ac61c2345381c53d8afee160e518
Spike Vinz Cruz <spikevinz@gmail.com>
2024-04-18 15:02:01 +0800

    add snake case validation for location name

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,5 +4,15 @@ class Location < ApplicationRecord
   include Weatherable
 
   validates_presence_of :name, :lat, :lon
+  validate :name_snake_case
+
   has_many :weathers
+
+  private
+
+  def name_snake_case
+    return if name =~ /\A[a-z0-9_]+\z/
+
+    errors.add(:name, 'must be in snake_case format')
+  end
 end
