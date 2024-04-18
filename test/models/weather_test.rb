0c183a9af16877d5cc5608e06dbf03d20e995217
Spike Vinz Cruz <spikevinz@gmail.com>
2024-04-18 15:09:41 +0800

    add unit test for weather poro

--- test/models/weather_test.rb
+++ test/models/weather_test.rb
@@ -1,7 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class WeatherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @weather = Weather.new(
+      code: 0,
+      date: '2024-04-18',
+      temperature: 25.0,
+      wind: 6.9
+    )
+  end
+
+  test 'should return correct day of the week' do
+    assert_equal 'Thursday', @weather.day
+  end
+
+  test 'should return correct weather description' do
+    assert_equal 'Sunny', @weather.description
+  end
+
+  test 'should return correct icon URL' do
+    assert_equal 'http=>//openweathermap.org/img/wn/01d@2x.png', @weather.icon_url
+  end
 end
